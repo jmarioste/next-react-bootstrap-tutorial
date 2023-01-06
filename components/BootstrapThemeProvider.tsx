dc659580cc64424ee3cd2a17f62c26899898a50f
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-01-06 20:35:26 +0800

    Add comments

--- components/BootstrapThemeProvider.tsx
+++ components/BootstrapThemeProvider.tsx
@@ -1,18 +1,21 @@
 import React, { PropsWithChildren, useContext, useEffect } from "react";
 import { useLocalStorage } from "usehooks-ts";
 
+// 👇 define a map of all the themes we'll be using in the app
 const ThemeMap = {
   main: "/styles/main.css",
   custom: "/styles/custom.css",
   cerulean: "https://bootswatch.com/5/cerulean/bootstrap.min.css",
   cosmo: "https://bootswatch.com/5/cosmo/bootstrap.min.css",
 };
+
 type ThemeKey = keyof typeof ThemeMap;
 
 type ThemeContextState = {
   theme: ThemeKey;
   setTheme(theme: ThemeKey): void;
 };
+
 const ThemeContext = React.createContext<ThemeContextState>({
   theme: "main",
   setTheme: () => {},
@@ -21,6 +24,7 @@ const ThemeContext = React.createContext<ThemeContextState>({
 const BootstrapThemeProvider = (props: PropsWithChildren) => {
   const [theme, setTheme] = useLocalStorage<ThemeKey>("theme", "main");
 
+  // 👇 when the theme changes we'll append a new link element pointing to the css url.
   useEffect(() => {
     if (theme) {
       //create a link element
@@ -34,7 +38,7 @@ const BootstrapThemeProvider = (props: PropsWithChildren) => {
       if (prevTheme) {
         document.head.removeChild(prevTheme);
       }
-      //if theme is not main we append the link
+      //if theme is not "main" we append the link
       if (theme !== "main") {
         document.head.append(themesheet);
       }
